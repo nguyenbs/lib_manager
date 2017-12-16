6c971be57555641898ac3c96248e245a807ad9dc
thien <thiennx@newwave.vn>
2017-12-16 16:37:39 +0700

    update gemfile/gemlock

--- config/deploy.rb
+++ config/deploy.rb
@@ -49,8 +49,8 @@ set :branch, :develop
 
 set :use_sudo, false
 set :bundle_binstubs, nil
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml')
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+#set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 after 'deploy:publishing', 'deploy:restart'
 
