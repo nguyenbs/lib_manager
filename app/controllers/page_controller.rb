40e258ac7b99fc910115716167749fe7e1883555
Quoc Tien <quoctien06091995@gmail.com>
2017-12-21 18:32:59 +0700

    feature search book by category

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,4 +1,16 @@
 class PageController < ApplicationController
   def index
+    @books = Book.all.limit(8)
+  end
+
+  def search
+    @books = Book.search_book(params[:search][:name], params[:search][:author], params[:search][:category])
+    binding.pry
+		if @books
+      redirect_to items_path(@books)
+    else
+      flash[:danger] = 'Book not exist'
+      redirect_to root_path
+    end
   end
 end
