39982976d0ee903d396b2314389e6529e44a74ea
thien <thiennx@newwave.vn>
2017-12-23 16:09:04 +0700

    fix bug deploy

--- db/migrate/20171222041742_add_devise_to_admins.rb
+++ db/migrate/20171222041742_add_devise_to_admins.rb
@@ -34,8 +34,6 @@ class AddDeviseToAdmins < ActiveRecord::Migration[5.1]
       # t.timestamps null: false
     end
 
-    add_index :admins, :email,                unique: true
-    add_index :admins, :reset_password_token, unique: true
     # add_index :admins, :confirmation_token,   unique: true
     # add_index :admins, :unlock_token,         unique: true
   end
