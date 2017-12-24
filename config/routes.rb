bd36d033dec9dca848062fc14a79251a2ed88ebe
thien <thiennx@newwave.vn>
2017-12-24 14:02:07 +0700

    add function logout for user

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  resources :users
   resources :items, only: [:index, :show]
 
   post 'items/search', to: 'items#search'
